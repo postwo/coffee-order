217aae5ece4f3e03f7dce4d8c17f030be201d8e1
postwo <codo7717@naver.com>
2025-02-10 18:03:51 +0900

    Store 도메인 추가

---
+++ src/main/java/com/example/coffee_order/domain/Store.java
@@ -0,0 +1,28 @@
+package com.example.coffee_order.domain;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Column;
+import org.springframework.data.relational.core.mapping.Table;
+
+import java.time.ZonedDateTime;
+
+@Table(name = "stores")
+public class Store {
+    @Id
+    private String storeId;
+
+    @Column
+    private String name;
+
+    @Column
+    private String address;
+
+    @Column
+    private String phoneNumber;
+
+    @Column
+    private ZonedDateTime openAt;
+
+    @Column
+    private ZonedDateTime closeAt;
+}
